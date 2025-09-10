c1ad57d53b00b58a32ac11a4561472683831a749
Ephraim-G <ephraim.gross@gsa.gov>
2025-09-10 18:07:28 -0400

    chore: Remove script

--- uploads/rename2.sh
+++
@@ -1,8 +0,0 @@
-for file in *.pdf; do
-
-  new_file=$(echo "$file" | sed -E 's/(__|_-_|-_)/_/g')
-  if [ "$file" != "$new_file" ]; then
-  mv "$file" "$new_file"
-  echo "Renamed: $file -> $new_file"
-  fi
-done
